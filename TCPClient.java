c6d26cbe18d97ea56e16df8f37a369628c0f67d7
ChangYuMine <king09372000@gmail.com>
2016-09-19 22:00:34 +0800

    Add files via upload

--- TCPClient.java
+++ TCPClient.java
@@ -1,6 +1,7 @@
-package tw.com.google.www._0904;
+package tw.com.google.www_0904;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -15,6 +16,14 @@ public class TCPClient {
 				= new Socket
 				
 					(InetAddress.getByName("127.0.0.1"),8888);
+			
+			OutputStream out = socket.getOutputStream();
+			
+			out.write("天佑台灣".getBytes());
+			
+			out.flush();
+			
+			out.close();
 			
 			socket.close();
 			
