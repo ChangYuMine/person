ce6f1621d1b3531f5227aefb18d0a1d0c925c6c7
ChangYuMine <king09372000@gmail.com>
2016-09-20 10:58:52 +0800

    Add files via upload

--- TCPClient.java
+++ TCPClient.java
@@ -1,5 +1,9 @@
 package tw.com.google.www_0904;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetAddress;
@@ -10,29 +14,100 @@ public class TCPClient {
 
 	public static void main(String[] args) {
 		
-		try {
-			Socket socket 
-			
-				= new Socket
+		long start = System.currentTimeMillis();
+		//顯示傳送時間
+		
+	try {
+		
+		Socket client = new Socket
 				
-					(InetAddress.getByName("127.0.0.1"),8888);
+				(InetAddress.getByName("127.0.0.1"),6666);
+		
+		//OutputStream out = client.getOutputStream();
+		//上式使用BufferedOutputStream後即可不用。
+		
+		BufferedOutputStream bout
+		
+				= new BufferedOutputStream
+				
+						(client.getOutputStream());
+		
+		BufferedInputStream bin = 
+				
+				new BufferedInputStream
+				
+					(new FileInputStream("dir/747.jpg"));
+		
+		int a;
+		
+		while((a=bin.read())!=-1){
 			
-			OutputStream out = socket.getOutputStream();
-			
-			out.write("天佑台灣".getBytes());
-			
-			out.flush();
-			
-			out.close();
-			
-			socket.close();
-			
-		} catch (UnknownHostException e) {
-			
-			
-		} catch (IOException e) {
+			bout.write(a);
 			
 		}
+		
+		bout.flush();
+		
+		bin.close();
+		
+		client.close();
+		
+		bout.close();
+		
+		System.out.println("sendOK");
+		
+		System.out.println(System.currentTimeMillis()-start);
+		//印出傳送時間
+		
+	} catch (FileNotFoundException e) {
+		
+		
+	} catch (IOException e) {
+		
+		System.out.println(e.toString());
+		
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//傳送訊息範例		
+//		try {
+//			Socket socket 
+//			
+//				= new Socket
+//				
+//					(InetAddress.getByName("127.0.0.1"),8888);
+//			
+//			OutputStream out = socket.getOutputStream();
+//			
+//			out.write("天佑台灣".getBytes());
+//			
+//			out.flush();
+//			
+//			out.close();
+//			
+//			socket.close();
+//			
+//		} catch (UnknownHostException e) {
+//			
+//			
+//		} catch (IOException e) {
+//			
+//		}
 		
 		
 
--- TCPReceiver.java
+++ TCPReceiver.java
@@ -1,51 +1,126 @@
 package tw.com.google.www_0904;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TCPReceiver {
 
 	public static void main(String[] args) {
-		
+				
 		try {
-			ServerSocket server 
 			
-					= new ServerSocket(8888);
+			ServerSocket server;
+			
+			server = new ServerSocket(6666);
 			
 			Socket socket = server.accept();
-			//重要!!接收動作!!
 			
-			InputStream in = socket.getInputStream();
+			BufferedInputStream bin=
+					
+					new BufferedInputStream
+					
+						(socket.getInputStream());
 			
-			InputStreamReader irs = new InputStreamReader(in);
+			BufferedOutputStream bout = 
+					
+					new BufferedOutputStream
+					
+						(new FileOutputStream("upload/747.jpg"));
 			
-			BufferedReader read = new BufferedReader(irs);
+			int a;
 			
-			int c;
-			
-			StringBuffer sb = new StringBuffer();
-			
-			while((c=read.read())!=-1){
+			while((a=bin.read())!=-1){
 				
-				sb.append((char)c);
-				
+				bout.write(a);
 			}
 			
-			InetAddress urip = socket.getInetAddress();
+			bout.flush();
+			
+			bin.close();
 			
 			server.close();
 			
-			System.out.println(urip.getHostAddress()+":"+sb);
-			
+			bout.close();
+						
+			System.out.println("RecOK");
+		
 		} catch (IOException e) {
-			
+		
+			System.out.println("Server"+e.toString());
 			
 		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//傳送訊息範例			
+//		try {
+//			ServerSocket server 
+//			
+//					= new ServerSocket(8888);
+//			
+//			Socket socket = server.accept();
+//			//重要!!接收動作!!
+//			
+//			InputStream in = socket.getInputStream();
+//			
+//			InputStreamReader irs = new InputStreamReader(in);
+//			
+//			BufferedReader read = new BufferedReader(irs);
+//			
+//			int c;
+//			
+//			StringBuffer sb = new StringBuffer();
+//			
+//			while((c=read.read())!=-1){
+//				
+//				sb.append((char)c);
+//				
+//			}
+//			
+//			InetAddress urip = socket.getInetAddress();
+//			
+//			server.close();
+//			
+//			System.out.println(urip.getHostAddress()+":"+sb);
+//			
+//		} catch (IOException e) {
+//			
+//			
+//		}
 
 	}
 
